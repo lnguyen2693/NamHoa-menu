b7ce51059f42df26f1e8e77273d7986911d53fc3
nickbar01234 <nickbar01234@gmail.com>
2025-01-08 13:12:33 +0700

    Cleanup some styling

--- src/components/menu/item/MenuItemCard.tsx
+++ src/components/menu/item/MenuItemCard.tsx
@@ -1,22 +1,13 @@
 import React from "react";
 import { IdentifiableMenuItem } from "@interfaces/type";
-import Card from "@mui/material/Card";
-import { styled } from "@mui/material";
-import { pink } from "@mui/material/colors";
+import { Paper, Typography } from "@mui/material";
 import { MenuItemDrawer } from "./MenuItemDrawer";
 import { Box } from "@mui/system";
+import Image from "next/image";
 
 interface menuItemCardProps {
   menuItem: IdentifiableMenuItem;
 }
-
-const CustomCard = styled(Card)({
-  width: "100%",
-  height: "auto",
-  position: "relative",
-  aspectRatio: 1,
-});
-
 const MenuItemCard = (props: menuItemCardProps) => {
   const { menuItem } = props;
   const priceInVnd = new Intl.NumberFormat("vi-VN", {
@@ -27,33 +18,43 @@ const MenuItemCard = (props: menuItemCardProps) => {
     .format(menuItem.price)
     .replace("VND", "");
 
+  console.log(menuItem.image);
   return (
     <Box>
-      <CustomCard>
-        <Box
-          style={{
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            backgroundImage: `url("${menuItem.image}")`,
-            width: "100%",
-            height: "100%",
-            opacity: menuItem.available ? 1 : 0.5
-          }}
-        >
-          <MenuItemDrawer
-            key={menuItem.id}
-            menuItem={menuItem}
-          ></MenuItemDrawer>
-        </Box>
-      </CustomCard>
+      <Paper
+        elevation={3}
+        style={{
+          position: "relative",
+          borderRadius: "5px",
+          overflow: "hidden",
+        }}
+      >
+        <Image
+          src={menuItem.image ?? ""}
+          alt={menuItem.name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "100%", height: "auto", display: "block" }}
+        />
+        <MenuItemDrawer key={menuItem.id} menuItem={menuItem} />
+      </Paper>
       <Box marginTop="8px">
-        <Box color={menuItem.available ? "#5DC389" : "#F35362"} fontSize="14px">
-          <span>{menuItem.available ? "Còn món" : "Hết món"}</span>
-        </Box>
-        <Box marginTop="4px" display="flex" flexDirection="column" rowGap="6px">
-          <span>{menuItem.name}</span>
-          <span style={{ fontWeight: 400 }}>{priceInVnd}đ</span>
+        <Typography
+          color={menuItem.available ? "#5DC389" : "#F35362"}
+          fontSize="14px"
+        >
+          {menuItem.available ? "Còn món" : "Hết món"}
+        </Typography>
+        <Box
+          marginTop="4px"
+          display="flex"
+          flexDirection="column"
+          color="#000000DE"
+          fontSize="14px"
+        >
+          <Typography fontWeight={500}>{menuItem.name}</Typography>
+          <Typography>{priceInVnd}đ</Typography>
         </Box>
       </Box>
     </Box>
