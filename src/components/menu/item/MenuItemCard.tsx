a61bc96b7f52d23790286c6c02255f05859b3ed0
nickbar01234 <nickbar01234@gmail.com>
2025-01-08 14:51:56 +0700

    Fix image

--- src/components/menu/item/MenuItemCard.tsx
+++ src/components/menu/item/MenuItemCard.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { IdentifiableMenuItem } from "@interfaces/type";
-import { Paper, Typography } from "@mui/material";
+import { Card, Paper, Typography } from "@mui/material";
 import { MenuItemDrawer } from "./MenuItemDrawer";
-import { Box } from "@mui/system";
+import { Box, styled } from "@mui/system";
 import Image from "next/image";
 
 interface menuItemCardProps {
   menuItem: IdentifiableMenuItem;
 }
+
+const CustomCard = styled(Card)({
+  width: "100%",
+  height: "auto",
+  position: "relative",
+  aspectRatio: 1,
+});
+
 const MenuItemCard = (props: menuItemCardProps) => {
   const { menuItem } = props;
   const priceInVnd = new Intl.NumberFormat("vi-VN", {
@@ -21,24 +29,21 @@ const MenuItemCard = (props: menuItemCardProps) => {
   console.log(menuItem.image);
   return (
     <Box>
-      <Paper
-        elevation={3}
-        style={{
-          position: "relative",
-          borderRadius: "5px",
-          overflow: "hidden",
-        }}
-      >
-        <Image
-          src={menuItem.image ?? ""}
-          alt={menuItem.name}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto", display: "block" }}
-        />
-        <MenuItemDrawer key={menuItem.id} menuItem={menuItem} />
-      </Paper>
+      <CustomCard elevation={4}>
+        <Box
+          style={{
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundImage: `url("${menuItem.image}")`,
+            width: "100%",
+            height: "100%",
+            opacity: menuItem.available ? 1 : 0.5,
+          }}
+        >
+          <MenuItemDrawer key={menuItem.id} menuItem={menuItem} />
+        </Box>
+      </CustomCard>
       <Box marginTop="8px">
         <Typography
           color={menuItem.available ? "#5DC389" : "#F35362"}
